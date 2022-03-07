976272b8e70114f51c4ba19db54a3b91a848b7cc
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2022-03-07 18:04:43 +0300

    Add update and delete People

--- js/people.js
+++ js/people.js
@@ -1,36 +1,73 @@
-const getUsers = async () => {
-    let response = await fetch('http://localhost:8080/api/people/get')
+const apiUrl = "http://localhost:8080/api/people"
+let id
+const getUsers = async (url) => {
+    let response = await fetch(url)
     return response.ok ? response.json() : undefined
 }
 
 const getPeople = async () => {
-    let users = await getUsers()
+    let users = await getUsers(`${apiUrl}/get`)
     console.log(users)
     users.forEach(user => {
+        console.log(user)
         let div = document.createElement('div')
         let divFio = document.createElement('div')
         let divAddress = document.createElement('div')
         let divPhone = document.createElement('div')
-        let divPersonalNumber = document.createElement('div')
+        let divPersonnelNumber = document.createElement('div')
+        let divUpdate = document.createElement('div')
+        let imgUpdate = document.createElement('img')
+        let divDelete = document.createElement('div')
+        let imgDelete = document.createElement('img')
         div.className = 'divTableRow'
         divFio.className = 'divTableCell'
         divAddress.className = 'divTableCell'
         divPhone.className = 'divTableCell'
-        divPersonalNumber.className = 'divTableCell'
+        divPersonnelNumber.className = 'divTableCell'
+        divUpdate.className = 'divTableCell'
+        imgUpdate.src = '../images/update.png'
+        divDelete.className = 'divTableCell'
+        imgDelete.src = '../images/delete.png'
+
 
         divFio.innerHTML = user.surName + ' ' + user.firstName + ' ' + user.patronymic
         divAddress.innerHTML = user.address
         divPhone.innerHTML = user.phoneNumber
-        if (user.personalNumber === undefined) {
-            divPersonalNumber.innerHTML = ''
+        if (user.personnelNumber === undefined) {
+            divPersonnelNumber.innerHTML = ''
         } else {
-            divPersonalNumber.innerHTML = user.personalNumber
+            divPersonnelNumber.innerHTML = user.personnelNumber
         }
-
+        imgUpdate.id = user.id
+        imgUpdate.onclick = async () => {
+            let person = await getUsers(`${apiUrl}/getbyid?id=${imgUpdate.id}`).then()
+            id = person.id
+            document.querySelector('.surName').value = person.surName
+            document.querySelector('.firstName').value = person.firstName
+            document.querySelector('.patronymic').value = person.patronymic
+            document.querySelector('.address').value = person.address
+            document.querySelector('.phoneNumber').value = person.phoneNumber
+            if (person.personnelNumber === undefined) {
+                document.querySelector('.personnelNumber').value = ''
+            } else {
+                document.querySelector('.personnelNumber').value = person.personnelNumber
+            }
+        }
+        imgDelete.id = user.id
+        imgDelete.onclick = async () => {
+            let responseDelete = await fetch(`${apiUrl}/delete?id=${imgUpdate.id}`)
+            // location.reload()
+            console.log(responseDelete)
+        }
         div.appendChild(divFio)
         div.appendChild(divAddress)
         div.appendChild(divPhone)
-        div.appendChild(divPersonalNumber)
+        div.appendChild(divPersonnelNumber)
+
+        div.appendChild(divUpdate)
+        divUpdate.appendChild(imgUpdate)
+        div.appendChild(divDelete)
+        divDelete.appendChild(imgDelete)
         document.querySelector('.divTableBody').appendChild(div)
     })
 }
@@ -38,6 +75,7 @@ const getPeople = async () => {
 document.forms.createPerson.onsubmit = async (event) => {
     let elements = event.target.elements
     let person = JSON.stringify({
+        id: id,
         surName: elements.surName.value,
         firstName: elements.firstName.value,
         patronymic: elements.patronymic.value,
@@ -45,7 +83,8 @@ document.forms.createPerson.onsubmit = async (event) => {
         phoneNumber: elements.phoneNumber.value,
         personnelNumber: elements.personnelNumber.value
     })
-    const response = await fetch('http://localhost:8080/api/people/create', {
+    console.log(person)
+    const response = await fetch(`${apiUrl}/create`, {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: person
